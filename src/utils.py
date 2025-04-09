02a3abff9befda2b7f33d9214aceed1c1dcae4aa
coderkush <codeai424@gmail.com>
2025-04-09 18:09:00 +0545

    Web app made

--- src/utils.py
+++ src/utils.py
@@ -4,9 +4,11 @@ import sys
 import numpy as np
 import pandas as pd
 from src.exception import CustomException
-import dill
+import pickle
 
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
+from src.logger import logging
 
 def save_object(file_path, obj):
     """Saves the model in a pickle '.pkl' file"""
@@ -15,22 +17,28 @@ def save_object(file_path, obj):
         os.makedirs(dir_path, exist_ok=True)
 
         with open(file_path, 'wb') as file_obj:
-            dill.dump(obj, file_obj) # save obj in file_obj
+            pickle.dump(obj, file_obj) # save obj in file_obj
 
 
     except Exception as e:
         raise CustomException(e, sys)
     
 
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+def evaluate_models(X_train, y_train, X_test, y_test, models, param):
     """Trains and evaluates a list of models"""
     try:
         report={}
+        logging.info("Training and evaluating the model")
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            para = param[list(models.keys())[i]]
+
+            gs = GridSearchCV(model, para, cv=3)
+            gs.fit(X_train, y_train)
             
-            model.fit(X_train, y_train) # Train model
-            
+            model.set_params(**gs.best_params_)
+            model.fit(X_train, y_train)
+
             y_train_pred = model.predict(X_train)
 
             y_test_pred = model.predict(X_test)
@@ -40,7 +48,14 @@ def evaluate_models(X_train, y_train, X_test, y_test, models):
             test_model_score = r2_score(y_true=y_test, y_pred=y_test_pred)
 
             report[list(models.keys())[i]] = test_model_score
-
         return report
+    except Exception as e:
+        raise CustomException(e, sys)
+
+def load_object(file_path):
+    try:
+        with open(file_path, 'rb') as file_obj:
+            return pickle.load(file_obj)
+        
     except Exception as e:
         raise CustomException(e, sys)
